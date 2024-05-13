3f7e5617f6d09e3c8a458768b87614e589c5f50c
kimgyeongmin5348 <135432080+kimgyeongmin5348@users.noreply.github.com>
2024-05-14 01:12:59 +0900

    완

--- Player.cpp
+++ Player.cpp
@@ -8,16 +8,50 @@ Player::~Player() {
 
 }
 
-/*Player(const Player& other)
-		: name(other.name), score(other.score), id(other.id), num(other.num) {
-		if (other.p) {
-			p = make_unique<char[]>(other.num);
-			copy(other.p.get(), other.p.get() + other.num, p.get());
-		}
-	}*/
+// 복사 생성자
+Player::Player(const Player& other)
+	: name(other.name), score(other.score), id(other.id), num(other.num)
+{
+	p = make_unique<char[]>(other.num);
+	copy(other.p.get(), other.p.get() + num, p.get());
+}
 
-bool Player::operator==(const Player& rhs)const {
-	return getScore() == rhs.getScore();
+// 복사 대입 연산자.
+Player& Player::operator=(const Player& other)
+{
+	if (this != &other)
+	{
+		name = other.name;
+		score = other.score;
+		id = other.id;
+		num = other.num;
+		p = std::make_unique<char[]>(other.num);
+		std::copy(other.p.get(), other.p.get() + num, p.get());
+	}
+	return *this;
+}
+
+// 이동 생성자
+Player::Player(Player&& other) : name(other.name), score(other.score), id(other.id), num(other.num)
+{
+	p.reset(other.p.get());
+	other.p.release();
+	other.num = 0;
+}
+
+// 이동 할당 연산자
+Player& Player::operator=(Player&& other)
+{
+	if (this != &other)
+	{
+		name = std::move(other.name);
+		score = other.score;
+		id = other.id;
+		num = other.num;
+		p.reset(other.p.release());
+		other.num = 0;
+	}
+	return *this;
 }
 
 string Player::getName() const {
@@ -36,19 +70,21 @@ size_t Player::getNum() const {
 	return num;
 }
 
-char* Player::getP() const{
-	return p;
+char* Player::getP() const {
+	return p.get();
 }
 
 bool Player::read(istream& is)
 {
+	
 	is.read((char*)this, sizeof Player);
-	p = new char[num];
-	is.read((char*)p, num);
-	delete p;
+	p.release();
+	p = make_unique<char[]>(num);
+	is.read(p.get(), num);
 
 	return bool(is);
 }
+
 
 void Player::show() const {
 	cout << "이름: " << name << ", 아이디: " << id << ", 점수: " << score << ", 자원수: " << num << '\n' << "저장된 글자 : " << p << '\n';
